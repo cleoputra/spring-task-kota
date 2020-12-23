ec6d962e746c3e5828b0b6d138636e18b23b21d8
cleo <acesam222@gmail.com>
2020-12-23 10:27:07 +0700

    update delete

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,26 @@
-/**
- * 
- */
+var tablePerson = {
+create: function(){
+		$.ajax({
+			url : '/provinsi/get-all',
+			method : 'get',
+			contetType : 'application/json',
+			success : function(res, status, xhr) {
+				data = "";
+				$.each(res, function(i, v) {
+					data += "<tr>";
+					data += "<td>" + v.id + "</td>";
+					data += "<td>" + v.kodeProvinsi + "</td>";
+					data += "<td>" + v.namaProvinsi + "</td>";
+					data += "<td>" + v.status + "</td>";
+					data += "<tr>";
+				});
+				$('body-person').append(data);
+				console.log(res);
+			},
+		error : function(e){
+			alert(e);
+			console.log(e);
+		}
+		});
+		}
+}		
